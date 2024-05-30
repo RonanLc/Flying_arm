a9c58dfb92268406713c281b288ceea5bcd2104e
Ronan Le Corronc <lecorroncronan@gmail.com>
2024-05-30 21:07:35 +0200

    Batch script test

--- code_test_DMA/Core/Inc/stm32f4xx_hal_conf.h
+++ code_test_DMA/Core/Inc/stm32f4xx_hal_conf.h
@@ -1,4 +1,23 @@
 /* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32f4xx_hal_conf_template.h
+  * @author  MCD Application Team
+  * @brief   HAL configuration template file.
+  *          This file should be copied to the application folder and renamed
+  *          to stm32f4xx_hal_conf.h.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2017 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
@@ -45,8 +64,8 @@
 /* #define HAL_MMC_MODULE_ENABLED */
 /* #define HAL_SPI_MODULE_ENABLED */
 /* #define HAL_TIM_MODULE_ENABLED */
-/* #define HAL_UART_MODULE_ENABLED */
-/* #define HAL_USART_MODULE_ENABLED */
+#define HAL_UART_MODULE_ENABLED
+#define HAL_USART_MODULE_ENABLED
 /* #define HAL_IRDA_MODULE_ENABLED */
 /* #define HAL_SMARTCARD_MODULE_ENABLED */
 /* #define HAL_SMBUS_MODULE_ENABLED */
@@ -474,5 +493,3 @@
 #endif
 
 #endif /* __STM32F4xx_HAL_CONF_H */
-#include "stm32f4xx_hal_uart.c" 
-#include "stm32f4xx_hal_usart.c" 
--- code_test_DMA/Drivers/Action/include_files.cmd
+++ code_test_DMA/Drivers/Action/include_files.cmd
@@ -2,6 +2,23 @@
 
 set "HALversion=1.28.0"
 
+set "composant[0]=uart"
+set "composant[1]=usart"
+set "composant[2]=adc"
+set "composant[3]=gpio"
+
+set "nombreComposant=4"
+
+
+
+
+
+
+
+
+
+
+
 set "HALSrcPath=%USERPROFILE%\STM32Cube\Repository\STM32Cube_FW_F4_V%HALversion%\Drivers\STM32F4xx_HAL_Driver\Src\"
 set "HALIncPath=%USERPROFILE%\STM32Cube\Repository\STM32Cube_FW_F4_V%HALversion%\Drivers\STM32F4xx_HAL_Driver\Inc\"
 
@@ -11,9 +28,48 @@ copy "%HALIncPath%stm32f4xx_hal_usart.h" "..\STM32F4xx_HAL_Driver\Inc"
 copy "%HALSrcPath%stm32f4xx_hal_uart.c" "..\STM32F4xx_HAL_Driver\Src"
 copy "%HALIncPath%stm32f4xx_hal_uart.h" "..\STM32F4xx_HAL_Driver\Inc"
 
-set "filePath=..\..\Core\Inc\stm32f4xx_hal_conf.h"
-set "Includefile1=#include "stm32f4xx_hal_uart.c""
-set "Includefile2=#include "stm32f4xx_hal_usart.c""
 
-echo %Includefile1% >> %filePath%
-echo %Includefile2% >> %filePath%
+rem Définit les fichiers à traiter
+set "filePath=..\..\Core\Inc\"
+
+set "confFile=stm32f4xx_hal_conf.h"
+set "tempFile=temp_hal_conf.h"
+
+set "confPath=%filePath%%confFile%"
+set "tempPath=%filePath%%tempFile%"
+
+rem Définit les lignes à détecter et à remplacer
+set "ligne_a_remplacer=/* #define HAL_UART_MODULE_ENABLED */"
+set "nouvelle_ligne=#define HAL_UART_MODULE_ENABLED"
+
+rem Efface le fichier de sortie s'il existe déjà
+if exist "%tempPath%" del "%tempPath%"
+
+rem Désactiver l'expansion retardée
+setlocal disabledelayedexpansion
+
+rem Parcourir chaque ligne du fichier d'entrée
+for /f "tokens=1* delims=:" %%a in ('findstr /n "^" "%confPath%"') do (
+    
+    set "ligne=%%b"
+
+    rem Activer l'expansion retardée pour la comparaison et l'écriture
+    setlocal EnableDelayedExpansion
+    rem Comparer la ligne courante avec la ligne à remplacer
+    if "!ligne!"=="%ligne_a_remplacer%" (
+        rem Si la ligne correspond, écrire la nouvelle ligne
+        >> "%tempPath%" echo %nouvelle_ligne%
+    ) else (
+        rem Sinon, écrire la ligne courante inchangée
+        >> "%tempPath%" echo(!ligne!
+    )
+    endlocal
+)
+
+endlocal
+
+del "%confPath%"
+rename "%tempPath%" "%confFile%"
+
+rem Efface le fichier de sortie si jamais il existe toujours
+if exist "%tempPath%" del "%tempPath%"
