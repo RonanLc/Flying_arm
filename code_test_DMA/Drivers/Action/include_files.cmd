6a7d5c6d4a7cae026cf0f9b850825c5dc1f3e1df
RonanLc <lecorroncronan@gmail.com>
2024-06-05 17:16:56 +0200

    Update batch script for extern clock speed

--- code_test_DMA/Drivers/Action/include_files.cmd
+++ code_test_DMA/Drivers/Action/include_files.cmd
@@ -11,12 +11,15 @@ rem Le reste du script ne doit normalement pas être touché.
 rem Version de HAL utilisé, à modifier en cas de mise à jour
 set "HALversion=1.28.0"
 
-rem défini les types de fichier à ajouter au projet, à bien noter en minuscule
+rem Défini les types de fichier à ajouter au projet, à bien noter en minuscule
 set "composant[0]=uart"
 set "composant[1]=usart"
 
-rem défini le nombre de fichier à ajouter (lié directement aux variables d'au dessus)
+rem Défini le nombre de fichier à ajouter (lié directement aux variables d'au dessus)
 set "nombreComposant=2"
+
+rem Défini la vitesse de la clock externe (en Hz)
+set "hse_clock_speed=8000000"
 
 rem _________________________________________________________________________________________________________________________
 
@@ -67,20 +70,19 @@ for /L %%i in (0 1 %nombreComposant%) do (
 
     rem Parcourir chaque ligne du fichier d'entrée
     for /f "tokens=1* delims=:" %%a in ('findstr /n "^" "%confPath%"') do (
-        
+
         rem Lis la ligne suivante du fichier conf.h
         set "ligne=%%b"
 
         rem Réactive l'expansion retardée pour pouvoir utiliser les variables précédemment déclarées
         setlocal EnableDelayedExpansion
 
-        rem Comparer la ligne courante avec la ligne à remplacer
+        rem Comparer la ligne courante avec la ligne à remplacer        
         if "!ligne!"=="!ligne_a_remplacer!" (
-
             rem Si la ligne correspond, écrire la nouvelle ligne
             >> "%tempPath%" echo !nouvelle_ligne!
-        ) else (
 
+        ) else (
             rem Sinon, écrire la ligne courante inchangée
             >> "%tempPath%" echo(!ligne!
         )
@@ -97,8 +99,66 @@ for /L %%i in (0 1 %nombreComposant%) do (
 
 )
 
+rem Re-définition de la vitesse de la clock externe
+set "hse_clock_speed_define=#define HSE_VALUE"
+set "hse_clock_speed_8=  #define HSE_VALUE    %hse_clock_speed%U"
+
+setlocal DisableDelayedExpansion
+
+for /f "tokens=1* delims=:" %%a in ('findstr /n "^" "%confPath%"') do (
+
+    rem Lis la ligne suivante du fichier conf.h
+    set "ligne=%%b"
+
+    setlocal enabledelayedexpansion
+
+    rem Récupère les deux premiers mots de la ligne actuelle
+    for /f "tokens=1,2 delims= " %%a in ("!ligne!") do (
+        set "define_ligne=%%a %%b"
+    )
+
+    rem Vérifie si il est sur la ligne ou la vitesse de la clock est définie
+    if "!define_ligne!"=="!hse_clock_speed_define!" (
+        rem Si c'est le cas, il re-écrit cette ligne avec la vitesse souhaitée
+        >> "%tempPath%" echo !hse_clock_speed_8!
+
+    ) else (
+        rem Sinon il écrit la ligne lue
+        >> "%tempPath%" echo(!ligne!
+    )
+
+    endlocal
+)
+
+rem Supprime le fichier conf.h original par le fichier temporaire modifié
+del "%confPath%"
+
+rem Renomme le fichier temporaire en fichier conf.h pour qu'il puisse être utilisé par STMCubeIDE comme le fichier original
+rename "%tempPath%" "%confFile%"
+
 rem Efface le fichier de sortie si jamais il existe toujours (normalement pas executé)
 if exist "%tempPath%" del "%tempPath%"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 EXIT /b 0
 
